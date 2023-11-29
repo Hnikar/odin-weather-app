e5e304c9651ad246c86b8fa9e49afcde3307b0f9
Hnikar <user@example.com>
2023-11-29 20:55:49 +0100

    add weather details display

--- src/dom.js
+++ src/dom.js
@@ -1,14 +1,32 @@
 import fetchData from "./api";
 
 const DomManipulation = (() => {
+	const elements = [
+		,
+		{ class: ".wind", dataKey: "wind_kph" },
+		{ class: ".humidity", dataKey: "humidity" },
+		{ class: ".uvindex", dataKey: "uv" },
+		{ class: ".visibility", dataKey: "vis_km" },
+		{ class: ".cloudiness", dataKey: "cloud" },
+		{ class: ".chanceofrain", dataKey: "chanceofrain" },
+		{ class: ".sunrise", dataKey: "sunrise" },
+		{ class: ".sunset", dataKey: "sunset" },
+		{ class: ".moon", dataKey: "moon" },
+	];
+
 	let inputCity = "London";
 	let setData = async () => {
-		try {
-			let data = await fetchData(inputCity);
-			return data.current.cloud;
-		} catch (error) {
-			return error;
-		}
+		let data = await fetchData(inputCity);
+
+		elements.forEach(async (element) => {
+			try {
+				let domElement = document.querySelector(element.class);
+				domElement.textContent = data.current[element.dataKey];
+			} catch (error) {
+				console.log("Error updating element:", error);
+			}
+		});
+		console.log(data);
 	};
 
 	return { setData };
