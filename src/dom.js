83799925da0c528271736a8aa6556afbd83d5348
Hnikar <user@example.com>
2024-02-07 07:26:53 +0100

    Fix chance of rain display bug

--- src/dom.js
+++ src/dom.js
@@ -1,180 +1,179 @@
 import fetchData from "./api";
 
 const DomManipulation = (() => {
-	const currentWeatherElements = [
-		{ selector: "weather-temp", dataKey: "temp_c" },
-		{ selector: "weather-desc", dataKey: "condition" },
-		{ selector: "weather-feels-like-anchor", dataKey: "feelslike_c" },
+  const currentWeatherElements = [
+    { selector: "weather-temp", dataKey: "temp_c" },
+    { selector: "weather-desc", dataKey: "condition" },
+    { selector: "weather-feels-like-anchor", dataKey: "feelslike_c" },
 
-		{ selector: "wind", dataKey: "wind_kph" },
-		{ selector: "humidity", dataKey: "humidity" },
-		{ selector: "uvindex", dataKey: "uv" },
-		{ selector: "visibility", dataKey: "vis_km" },
-		{ selector: "cloudiness", dataKey: "cloud" },
-	];
-	//попробуй убрать slice приколы, тебе нужен один и тот же адресс
-	const forecastWeatherElements = [
-		[
-			{ selector: "chance-of-rain", dataKey: "daily_chance_of_rain-0" },
-			{ selector: "sunrise", dataKey: "sunrise-0" },
-			{ selector: "sunset", dataKey: "sunset-0" },
-			{ selector: "moon-phase", dataKey: "moon_phase-0" },
-		],
-		[
-			{ selector: "weekly-forecast-temp-1", dataKey: "avgtemp_c-1" },
-			{ selector: "weekly-forecast-wind-1", dataKey: "maxwind_kph-1" },
-			{
-				selector: "weekly-forecast-chance-of-rain-1",
-				dataKey: "daily_chance_of_rain-1",
-			},
-		],
-		[
-			{ selector: "weekly-forecast-temp-2", dataKey: "avgtemp_c-2" },
-			{ selector: "weekly-forecast-wind-2", dataKey: "maxwind_kph-2" },
-			{
-				selector: "weekly-forecast-chance-of-rain-2",
-				dataKey: "daily_chance_of_rain-2",
-			},
-		],
-	];
+    { selector: "wind", dataKey: "wind_kph" },
+    { selector: "humidity", dataKey: "humidity" },
+    { selector: "uvindex", dataKey: "uv" },
+    { selector: "visibility", dataKey: "vis_km" },
+    { selector: "cloudiness", dataKey: "cloud" },
+  ];
+  //попробуй убрать slice приколы, тебе нужен один и тот же адресс
+  const forecastWeatherElements = [
+    [
+      { selector: "chance-of-rain", dataKey: "daily_chance_of_rain-0" },
+      { selector: "sunrise", dataKey: "sunrise-0" },
+      { selector: "sunset", dataKey: "sunset-0" },
+      { selector: "moon-phase", dataKey: "moon_phase-0" },
+    ],
+    [
+      { selector: "weekly-forecast-temp-1", dataKey: "avgtemp_c-1" },
+      { selector: "weekly-forecast-wind-1", dataKey: "maxwind_kph-1" },
+      {
+        selector: "weekly-forecast-chance-of-rain-1",
+        dataKey: "daily_chance_of_rain-1",
+      },
+    ],
+    [
+      { selector: "weekly-forecast-temp-2", dataKey: "avgtemp_c-2" },
+      { selector: "weekly-forecast-wind-2", dataKey: "maxwind_kph-2" },
+      {
+        selector: "weekly-forecast-chance-of-rain-2",
+        dataKey: "daily_chance_of_rain-2",
+      },
+    ],
+  ];
 
-	const farenheitTempBtn = document.getElementById("farenheit-btn");
-	const celsiusTempBtn = document.getElementById("celsius-btn");
+  const farenheitTempBtn = document.getElementById("farenheit-btn");
+  const celsiusTempBtn = document.getElementById("celsius-btn");
 
-	farenheitTempBtn.addEventListener("click", async () => {
-		await _updateTemperatureUnit("temp_f", "feelslike_f");
-	});
+  farenheitTempBtn.addEventListener("click", async () => {
+    await _updateTemperatureUnit("temp_f", "feelslike_f");
+  });
 
-	celsiusTempBtn.addEventListener("click", async () => {
-		await _updateTemperatureUnit("temp_c", "feelslike_c");
-	});
+  celsiusTempBtn.addEventListener("click", async () => {
+    await _updateTemperatureUnit("temp_c", "feelslike_c");
+  });
 
-	const searchForm = document.getElementById("search-form");
-	const searchInput = document.getElementById("search-input");
+  const searchForm = document.getElementById("search-form");
+  const searchInput = document.getElementById("search-input");
 
-	const domWeatherElements = {};
-	function _updateDom() {
-		currentWeatherElements.forEach((element) => {
-			domWeatherElements[element.dataKey] = document.getElementById(
-				element.selector
-			);
-		});
-		forecastWeatherElements.forEach((day) => {
-			day.forEach((element) => {
-				domWeatherElements[element.dataKey] = document.getElementById(
-					element.selector
-				);
-			});
-		});
-	}
+  const domWeatherElements = {};
+  function _updateDom() {
+    currentWeatherElements.forEach((element) => {
+      domWeatherElements[element.dataKey] = document.getElementById(
+        element.selector
+      );
+    });
+    forecastWeatherElements.forEach((day) => {
+      day.forEach((element) => {
+        domWeatherElements[element.dataKey] = document.getElementById(
+          element.selector
+        );
+      });
+    });
+  }
 
-	searchForm.addEventListener("submit", async function (event) {
-		event.preventDefault();
-		let searchInputValue = searchInput.value;
-		await setData(searchInputValue);
-		searchForm.reset();
-	});
+  searchForm.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    let searchInputValue = searchInput.value;
+    await setData(searchInputValue);
+    searchForm.reset();
+  });
 
-	// Дописать для forecast avgtemp_c/f
-	let searchInputBuffer;
-	const _updateTemperatureUnit = async (unit, feelslikeUnit) => {
-		currentWeatherElements[0].dataKey = unit;
-		currentWeatherElements[2].dataKey = feelslikeUnit;
+  // Дописать для forecast avgtemp_c/f
+  let searchInputBuffer;
+  const _updateTemperatureUnit = async (unit, feelslikeUnit) => {
+    currentWeatherElements[0].dataKey = unit;
+    currentWeatherElements[2].dataKey = feelslikeUnit;
 
-		forecastWeatherElements.forEach((day, dayIndex) => {
-			day.forEach((element) => {
-				if (
-					element.dataKey.slice(0, -2) === "avgtemp_c" ||
-					element.dataKey.slice(0, -2) === "avgtemp_f"
-				) {
-					element.dataKey = `avg${unit}-${dayIndex}`;
-					console.log(element.dataKey);
-				}
-			});
-		});
+    forecastWeatherElements.forEach((day, dayIndex) => {
+      day.forEach((element) => {
+        if (
+          element.dataKey.slice(0, -2) === "avgtemp_c" ||
+          element.dataKey.slice(0, -2) === "avgtemp_f"
+        ) {
+          element.dataKey = `avg${unit}-${dayIndex}`;
+          console.log(element.dataKey);
+        }
+      });
+    });
 
-		_updateDom();
-		await setData(searchInputBuffer);
-	};
+    _updateDom();
+    await setData(searchInputBuffer);
+  };
 
-	const locationName = document.querySelector(".location-data");
-	const locationDateAndTime = document.querySelector(".date-and-time");
+  const locationName = document.querySelector(".location-data");
+  const locationDateAndTime = document.querySelector(".date-and-time");
 
-	async function _setCurrentWeather(data) {
-		currentWeatherElements.forEach(async (element) => {
-			try {
-				if (element.dataKey === "condition")
-					domWeatherElements[element.dataKey].textContent =
-						data.current.condition.text;
-				else
-					domWeatherElements[element.dataKey].textContent =
-						Math.round(data.current[element.dataKey]);
+  async function _setCurrentWeather(data) {
+    currentWeatherElements.forEach(async (element) => {
+      try {
+        if (element.dataKey === "condition")
+          domWeatherElements[element.dataKey].textContent =
+            data.current.condition.text;
+        else
+          domWeatherElements[element.dataKey].textContent = Math.round(
+            data.current[element.dataKey]
+          );
 
-				if (
-					element.dataKey === "temp_c" ||
-					element.dataKey === "feelslike_c"
-				)
-					domWeatherElements[element.dataKey].textContent += "°C";
-				else if (
-					element.dataKey === "temp_f" ||
-					element.dataKey === "feelslike_f"
-				)
-					domWeatherElements[element.dataKey].textContent += "°F";
-			} catch (error) {
-				console.log("Error updating current weather element:", error);
-			}
-		});
-	}
+        if (element.dataKey === "temp_c" || element.dataKey === "feelslike_c")
+          domWeatherElements[element.dataKey].textContent += "°C";
+        else if (
+          element.dataKey === "temp_f" ||
+          element.dataKey === "feelslike_f"
+        )
+          domWeatherElements[element.dataKey].textContent += "°F";
+      } catch (error) {
+        console.log("Error updating current weather element:", error);
+      }
+    });
+  }
 
-	async function _setForecast(data) {
-		forecastWeatherElements.forEach((day, dayIndex) => {
-			day.forEach((element) => {
-				try {
-					if (element.dataKey.slice(0, -2) === "daily_chance_of_rain")
-						domWeatherElements[element.dataKey].textContent =
-							data.forecast.forecastday[dayIndex].day[
-								element.dataKey.slice(0, -2)
-							] += "%";
-					if (dayIndex === 0) {
-						domWeatherElements[element.dataKey].textContent =
-							data.forecast.forecastday[dayIndex].astro[
-								element.dataKey.slice(0, -2)
-							];
-					} else
-						domWeatherElements[element.dataKey].textContent =
-							data.forecast.forecastday[dayIndex].day[
-								element.dataKey.slice(0, -2)
-							];
+  async function _setForecast(data) {
+    forecastWeatherElements.forEach((day, dayIndex) => {
+      day.forEach((element) => {
+        try {
+          if (dayIndex === 0) {
+            domWeatherElements[element.dataKey].textContent =
+              data.forecast.forecastday[dayIndex].astro[
+                element.dataKey.slice(0, -2)
+              ];
+            if (element.dataKey.slice(0, -2) === "daily_chance_of_rain") {
+              domWeatherElements[element.dataKey].textContent =
+                data.forecast.forecastday[dayIndex].day[
+                  element.dataKey.slice(0, -2)
+                ] += "%";
+            }
+          } else
+            domWeatherElements[element.dataKey].textContent =
+              data.forecast.forecastday[dayIndex].day[
+                element.dataKey.slice(0, -2)
+              ];
 
-					if (element.dataKey.slice(0, -2) === "avgtemp_c")
-						domWeatherElements[element.dataKey].textContent += "°C";
-					else if (element.dataKey.slice(0, -2) === "avgtemp_f")
-						domWeatherElements[element.dataKey].textContent += "°F";
-				} catch (error) {
-					console.log("Error updating forecast element:", error);
-				}
-			});
-		});
-	}
+          if (element.dataKey.slice(0, -2) === "avgtemp_c")
+            domWeatherElements[element.dataKey].textContent += "°C";
+          else if (element.dataKey.slice(0, -2) === "avgtemp_f")
+            domWeatherElements[element.dataKey].textContent += "°F";
+        } catch (error) {
+          console.log("Error updating forecast element:", error);
+        }
+      });
+    });
+  }
 
-	async function setData(inputCity) {
-		try {
-			_updateDom();
-			searchInputBuffer = inputCity;
-			let data = await fetchData(inputCity);
-			console.log(data);
-			if (!data.current) throw new Error(data);
-			locationName.textContent =
-				data.location.name + ", " + data.location.country;
-			locationDateAndTime.textContent = data.location.localtime;
-			_setCurrentWeather(data);
-			_setForecast(data);
-		} catch (error) {
-			console.log(error);
-		}
-	}
+  async function setData(inputCity) {
+    try {
+      _updateDom();
+      searchInputBuffer = inputCity;
+      let data = await fetchData(inputCity);
+      console.log(data);
+      if (!data.current) throw new Error(data);
+      locationName.textContent =
+        data.location.name + ", " + data.location.country;
+      locationDateAndTime.textContent = data.location.localtime;
+      _setCurrentWeather(data);
+      _setForecast(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	return { setData };
+  return { setData };
 })();
 
 export default DomManipulation;
