b34597b311b149507ecaae83e70b11a284756c4f
Hnikar <user@example.com>
2024-02-09 16:48:07 +0100

    add loading screen

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import DomManipulation from "./dom";
-DomManipulation.setData("Amsterdam");
+
+DomManipulation.setData("Amsterdam").then(() => {
+  document.getElementById("loading-screen").style.opacity = "0";
+  setTimeout(function () {
+    document.getElementById("loading-screen").style.display = "none";
+  }, 500);
+});
